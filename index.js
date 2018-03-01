d42e0924819939af302104e7f3c752a1c1f2019d
Bartosz Boron <bartosz.boron@maibornwolff.de>
2018-03-01 19:48:44 +0100

    init: example

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const fileService = require('./src/services/fileService');
+
+
+// DIST_MAP = determineDistancePerRide() -> 1->4; 2->2; 3->2
+
+let NUM_ROWS = 3;
+let NUM_COLS = 4;
+let NUM_VEHICLES = 2;
+let NUM_RIDES = 3;
+let NUM_TICKS = 10;
+
+let rides = [{
+    rowStart: 0,
+    columnStart: 0,
+    rowFinish: 1,
+    columnFinish: 3,
+    startTime: 2,
+    finishTime: 9,
+    complete: false,
+    vid: null
+}];
+
+
+// forEachTick (1...10)
+
+//   forEachFreeVehicles()
+
+//      r = findNextPossibleRide(remainingTicks, v.x, v.y)
+
+//      v.ride = r
+
+//   forEachAssignedVehicle() -> move() // xy updaten oder an der stelle bleiben
+
+//   forEachFinishedRide(r -> r.isCompleted())  // deassing vehicle
+
+
+
