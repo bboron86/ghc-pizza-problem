e649dada49f6f3d7ba1531bb7e5ccabaabfa862c
Bartosz Boron <bartosz.boron@maibornwolff.de>
2018-03-01 20:11:39 +0100

    foreach structure

--- index.js
+++ index.js
@@ -6,8 +6,54 @@ const fileLines = fileService.getFileLines(`./input/${FILE_NAME_WITHOUT_EXTENSIO
 
 // extract the first line
 const [NUM_ROWS, NUM_COLS, NUM_VEHICLES, NUM_RIDES, BONUS, NUM_TICKS] = fileLines.shift().split(' ');
-const rides = rideService.getAllRides(fileLines);
-// DIST_MAP = determineDistancePerRide() -> 1->4; 2->2; 3->2
+
+const ALL_RIDES = rideService.getAllRides(fileLines);
+const ALL_VEHICLES = [];
+
+
+
+for (let tickIdx = 0; tickIdx < NUM_TICKS; tickIdx++) {
+    
+    for (let vehicle of findAssignedVehicles()) {
+        moveOrWaitVehicle(vehicle, tickIdx);
+    }
+    
+    for (let finishedRide of findFinishedRides()) {
+        findAssignedVehicles()
+        .filter(v => v.rideId === finishedRide.id)
+        .forEach(v => {
+            v.completedRideIds.push(v.rideId);
+            v.rideId = null;
+        })
+    }
+    
+    for (let vehicle of findFreeVehicles()) {
+    
+        let nextRide = findNextPossibleRide(NUM_TICKS - 1 - tickIdx, vehicle.row, vehicle.col);
+        vehicle.rideId = nextRide.id;
+        nextRide.vid = vehicle.id;
+    }
+}
+
+function findFreeVehicles() {
+    return ALL_VEHICLES.filter(v => v.rideId === null);
+}
+
+function findAssignedVehicles() {
+    return ALL_VEHICLES.filter(v => v.rideId !== null);
+}
+
+function findFinishedRides() {
+    return ALL_RIDES.filter(r => r.complete === true);
+}
+
+function findNextPossibleRide(remainingTicks, vehicleRow, vehicleCol) {
+    // todo
+}
+
+function moveOrWaitVehicle(vehicle, tickIdx) {
+    // todo
+}
 
 // forEachTick (1...10)
 
